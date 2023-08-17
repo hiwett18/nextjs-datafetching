8c7c8323b2d561059bf52344990e74f375e9b701
Hiwot Woldesemayat <hiwot.woldesemayat@fdmgroup.com>
2023-08-16 23:27:30 -0400

    section 5

---
+++ data-fetching/pages/[uid].js
@@ -0,0 +1,16 @@
+function userIdPage(props) {
+    return <h1>{props.id}</h1>
+}
+
+export default userIdPage;
+
+export async function getServerSideProps(context) {
+    const { params } = context; 
+    const userId = params.uid; 
+
+    return {
+        props: {
+            id: 'userid-' + userId
+        }
+    }
+}
